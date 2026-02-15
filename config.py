335187fc384c49cff991130091f3f22c5aac9007
raistaridsell-boop <raistaridsell@gmail.com>
2026-02-16 00:03:27 +0530

    Update config.py
    Signed-off-by: raistaridsell-boop <raistaridsell@gmail.com>

--- config.py
+++ config.py
@@ -35,6 +35,11 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/xbitcode/music.git",
+)
+
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
